87df6b0bd8f5b6d00c02dff9a3e24561f51545a2
treda <tomekreda16@gmail.com>
2021-05-26 20:17:30 +0200

    Konfiguracji grafany

--- test-backend/src/main/java/pl/test/testbackend/grafana/config/GrafanaConfig.java
+++ test-backend/src/main/java/pl/test/testbackend/grafana/config/GrafanaConfig.java
@@ -1,4 +1,13 @@
 package pl.test.testbackend.grafana.config;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+
 public class GrafanaConfig {
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", "TESTAPP");
+    }
 }
--- test-backend/src/main/java/pl/test/testbackend/grafana/serivce/UserServices.java
+++ test-backend/src/main/java/pl/test/testbackend/grafana/serivce/UserServices.java
@@ -1,5 +1,6 @@
 package pl.test.testbackend.grafana.serivce;
 
+import com.codahale.metrics.annotation.Timed;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,12 +23,14 @@ public class UserServices {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
+    @Timed(name = "test")
     public ResponseEntity add(@RequestBody UserDto userDto) {
         User user = userMapper.mapToUser(userDto);
         userRepository.save(user);
         return ResponseEntity.ok().build();
     }
 
+    @io.micrometer.core.annotation.Timed
     public ResponseEntity delete(Long id) {
         Optional<User> user = userRepository.findById(id);
         if (user.isPresent()) {
